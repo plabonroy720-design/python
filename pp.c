1a63736df4e249df7fa5cc82aba3cebc3c5478c6
Plabon Roy <plabonroy720@gmail.com>
2026-06-30 20:48:22 +0600

    Update pp.c to calculate and display sum
    Modified the program to calculate the sum of numbers instead of printing messages.

--- pp.c
+++ pp.c
@@ -1,15 +1,13 @@
 #include <stdio.h>sdc
 int main ()
 {
-    int n;
+    int n,s;
     printf("Enter you count :");
     scanf("%d",&n);
     for(int i=1;i<=n;i++)
     {
-        printf("%d fuck you\n",i);
-        printf("%d yes baby\n",i);
-        printf("%d ahhhhhhh\n",i);
-    }
+    s+=i;
+        printf("The sum of the numbers is : %d ", s);
     return 0;
 
 }
