f068ab260eec5d098801d9ec81079317641491f0
Plabon Roy <plabonroy720@gmail.com>
2026-07-20 01:33:35 +0600

    Update phone number validation pattern

--- Number_Validation.py
+++ Number_Validation.py
@@ -1,6 +1,6 @@
 import re
 number=input("Enter the phone number : ")
-pattern=r'^(017|018|019|013)\d{8}$'
+pattern=r'^(013|014|015|016|017|018|019)\d{8}$'
 if re.fullmatch(pattern, number):
     print("The number is valid....")
 else:
